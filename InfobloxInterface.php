d1b18cc60347390a36c24e54dc0d70765c03e55c
Ben Marti <kareas1115@gmail.com>
2016-05-27 13:57:03 -0700

    finished createDNSEntry

--- InfobloxInterface.php
+++ InfobloxInterface.php
@@ -7,30 +7,63 @@
 	    public $username;
 	    public $password;
 
-	    function __construct($host, $user, $pass){
 
-	    	$hostname = $host;
-	    	$username = $user;
-	    	$password = $pass;
+	    public function getHostname(){
+
+
+
+	    }
+
+	    public function getUsername(){
+
+
+
+	    }
+
+	    public function getPassword(){
+
+
+
+	    }
+
+
+	    public function __construct($host, $user, $pass){
+
+	    	$this->hostname = $host;
+	    	$this->username = $user;
+	    	$this->password = $pass;
 
 	    }
 
 	    // method declaration
-	    public function createDNSEntry($url,$username,$password,$entry, $ip) {
+	    public function createDNSEntry($entry, $ip) {
 
 	        $ch = curl_init();   //init curl
 
+	        $url = $this->hostname . "/wapi/v1.2/record:host";
+
+	        $json = json_encode(array(
+				 "name" => "$entry",
+				 "ipv4addrs" => array(array(
+					"ipv4addr" => "$ip"
+				 ))
+			));  
+
 			curl_setopt($ch,CURLOPT_URL,$url); //define url
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 			curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Disable SSL verification
 			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // Disable SSL verification
 			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);  //http auth type basic
-			curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);  //UN and PW
+			curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);  //UN and PW
 		    //curl_setopt($ch,CURLOPT_HEADER, false);  //false to not return headers
+			curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+				'Content-Type: application/json',                                                                                
+				'Content-Length: ' . strlen($json))                                                                       
+			); 
 
 			$output['data'] = curl_exec($ch);
-			//$output['headers'] = curl_getinfo($ch);
+			$output['headers'] = curl_getinfo($ch);
 
 
 			curl_close($ch);
