e9ffc57f5b7e9a96976743609ebcbf18b6d09d0a
Ben Marti <kareas1115@gmail.com>
2016-06-02 23:17:31 -0400

    DELETE Function

--- InfobloxInterface.php
+++ InfobloxInterface.php
@@ -60,17 +60,38 @@
 
 	    ***/
 
-	    public function deleteDNSEntry($url,$username,$password,$entry) {
+	    public function deleteDNSEntry($entry) {
 
 	    	$ch = curl_init();   //init curl
 
+	    	$url = $this->hostname . "/wapi/v1.2/record:host?name~=" . $entry;
+
+
 			curl_setopt($ch,CURLOPT_URL,$url); //define url
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
 			curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
 			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Disable SSL verification
 			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // Disable SSL verification
 			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);  //http auth type basic
-			curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);  //UN and PW
+			curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);  //UN and PW
+		    //curl_setopt($ch,CURLOPT_HEADER, false);  //false to not return headers
+
+			$data['data'] = curl_exec($ch);
+			//$output['headers'] = curl_getinfo($ch);
+
+			$ipdata = $data['data'];
+			$ipdata = json_decode($ipdata,true);
+			$ref = $ipdata[0]["_ref"];
+
+
+			$url = "https://10.227.1.79/wapi/v1.2/" . $ref;
+
+			curl_setopt($ch, CURLOPT_URL,$url); //define url
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "DELETE");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER,true); //TRUE to return the transfer as a string of the return value of curl_exec() instead of outputting it out directly.
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);  // Disable SSL verification
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);  // Disable SSL verification
+			curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);  //http auth type basic
+			curl_setopt($ch, CURLOPT_USERPWD, $this->username . ":" . $this->password);  //UN and PW
 		    //curl_setopt($ch,CURLOPT_HEADER, false);  //false to not return headers
 
 			$output['data'] = curl_exec($ch);
